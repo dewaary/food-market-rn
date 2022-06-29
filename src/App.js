657b7a8060fc013238461c933309b3a28147c0cf
dewagedeagung <dewagede.septiyadi@ist.id>
2022-06-29 22:17:44 +0700

    Slicing UI Sign In

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import { SplashScreen } from './screens';
+import { SignIn, SplashScreen } from './screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const App = () => {
   return (
-    <SplashScreen />
+    <NavigationContainer>
+      <SignIn />
+    </NavigationContainer>
   );
 };
 
